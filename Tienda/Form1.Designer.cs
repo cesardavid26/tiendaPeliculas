e6ee131c4666338346011dae22c9f59e89a0adac
cesardavid26 <cesardavidcp@ufps.edu.co>
2020-11-16 14:18:30 -0500

    primer commit

---
+++ Tienda/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Tienda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.dgvPeliculas = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeliculas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(306, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TIENDA MUVI";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Género:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descripción:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Año:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(139, 103);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(119, 25);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(139, 148);
+            this.txtGenero.Multiline = true;
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(119, 25);
+            this.txtGenero.TabIndex = 6;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(140, 187);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(119, 36);
+            this.txtDescripcion.TabIndex = 7;
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(139, 244);
+            this.txtAño.Multiline = true;
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(119, 25);
+            this.txtAño.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Lime;
+            this.btnAgregar.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAgregar.Location = new System.Drawing.Point(30, 294);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(103, 38);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnBorrar.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBorrar.Location = new System.Drawing.Point(139, 294);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(103, 38);
+            this.btnBorrar.TabIndex = 10;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Yellow;
+            this.btnCancelar.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCancelar.Location = new System.Drawing.Point(248, 294);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(103, 38);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Limpiar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // dgvPeliculas
+            // 
+            this.dgvPeliculas.AllowUserToAddRows = false;
+            this.dgvPeliculas.AllowUserToDeleteRows = false;
+            this.dgvPeliculas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPeliculas.Location = new System.Drawing.Point(466, 103);
+            this.dgvPeliculas.Name = "dgvPeliculas";
+            this.dgvPeliculas.ReadOnly = true;
+            this.dgvPeliculas.Size = new System.Drawing.Size(412, 201);
+            this.dgvPeliculas.TabIndex = 12;
+            this.dgvPeliculas.DoubleClick += new System.EventHandler(this.dgvPeliculas_DoubleClick);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBuscar.Location = new System.Drawing.Point(666, 59);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(103, 31);
+            this.btnBuscar.TabIndex = 13;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(466, 63);
+            this.txtBuscar.Multiline = true;
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(194, 25);
+            this.txtBuscar.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 402);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgvPeliculas);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TIENDA DE PELICULAS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeliculas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.DataGridView dgvPeliculas;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
---
+++ Tienda/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Tienda
+{
+
+    public partial class Form1 : Form
+
+    {
+        string connectionString = @"Server=localhost;Database=tienda;Uid=root;Pwd=;";
+        int peliculaid = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            GridFill();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlcon = new MySqlConnection(connectionString))
+            {
+                mysqlcon.Open();
+                MySqlCommand mysqlcmd = new MySqlCommand("PeliculaAddOrEdit", mysqlcon);
+                mysqlcmd.CommandType = CommandType.StoredProcedure;
+                mysqlcmd.Parameters.AddWithValue("_idpelicula", peliculaid);
+                mysqlcmd.Parameters.AddWithValue("_nombre", txtNombre.Text.Trim());
+                mysqlcmd.Parameters.AddWithValue("_genero", txtGenero.Text.Trim());
+                mysqlcmd.Parameters.AddWithValue("_descripcion", txtDescripcion.Text.Trim());
+                mysqlcmd.Parameters.AddWithValue("_año", txtAño.Text.Trim());
+                mysqlcmd.ExecuteNonQuery();
+                MessageBox.Show("Registro exitoso");
+                Clear();
+                GridFill();
+            }
+        }
+        void GridFill()
+        {
+            using (MySqlConnection mysqlcon = new MySqlConnection(connectionString))
+            {
+                mysqlcon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("PeliculaViewAll", mysqlcon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dt = new DataTable();
+                sqlDa.Fill(dt);
+                dgvPeliculas.DataSource = dt;
+                dgvPeliculas.Columns[0].Visible = false;
+            }
+        }
+        void Clear()
+        {
+            txtNombre.Text = txtGenero.Text = txtDescripcion.Text = txtBuscar.Text = txtAño.Text = "";
+            peliculaid = 0;
+            btnAgregar.Text = "Agregar";
+            btnBorrar.Enabled = Enabled;
+        }
+
+        private void dgvPeliculas_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvPeliculas.CurrentRow.Index != -1)
+            {
+                txtNombre.Text = dgvPeliculas.CurrentRow.Cells[1].Value.ToString();
+                txtGenero.Text = dgvPeliculas.CurrentRow.Cells[2].Value.ToString();
+                txtDescripcion.Text = dgvPeliculas.CurrentRow.Cells[3].Value.ToString();
+                txtAño.Text = dgvPeliculas.CurrentRow.Cells[4].Value.ToString();
+                peliculaid = Convert.ToInt32(dgvPeliculas.CurrentRow.Cells[0].Value.ToString());
+                btnAgregar.Text = "Actualizar";
+            }
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlcon = new MySqlConnection(connectionString))
+            {
+                mysqlcon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("PeliculaSearchByValue", mysqlcon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("_SearchValue", txtBuscar.Text);
+                DataTable dt = new DataTable();
+                sqlDa.Fill(dt);
+                dgvPeliculas.DataSource = dt;
+                dgvPeliculas.Columns[0].Visible = false;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlcon = new MySqlConnection(connectionString))
+            {
+                mysqlcon.Open();
+                MySqlCommand mysqlcmd = new MySqlCommand("PeliculaDeleteById", mysqlcon);
+                mysqlcmd.CommandType = CommandType.StoredProcedure;
+                mysqlcmd.Parameters.AddWithValue("_peliculaid", peliculaid);
+                mysqlcmd.ExecuteNonQuery();
+                MessageBox.Show("Eliminacion exitosa");
+                Clear();
+                GridFill();
+            }
+        }
+    }
+}
